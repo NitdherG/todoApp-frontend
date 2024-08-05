2226a9f923b393a1490d921d6024fd0020fdf606
NitdherG <nitdherdev@gmail.com>
2024-08-05 15:49:47 -0400

    [frontend-todo] se crea el servicio para mostrar dialog

---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+  ],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss',
+})
+export class DialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string },
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
