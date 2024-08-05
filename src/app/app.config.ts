cdb422c1f6ee3557ff58957f96115c7da4b7af15
NitdherG <nitdherdev@gmail.com>
2024-08-05 16:13:50 -0400

    [frontend-todo] se agrega configuracion para firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideHttpClient } from "@angular/common/http";
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
+    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"todoapp-fbc7d","appId":"1:911067566463:web:36f8a404bf0e5bcba04a4a","storageBucket":"todoapp-fbc7d.appspot.com","locationId":"us-central","apiKey":"AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A","authDomain":"todoapp-fbc7d.firebaseapp.com","messagingSenderId":"911067566463"})), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
 };
