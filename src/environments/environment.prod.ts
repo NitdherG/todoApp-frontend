b83c55cf4b9c541d75bf8fa251bf905406cb5a83
NitdherG <nitdherdev@gmail.com>
2024-08-05 16:54:10 -0400

    [frontend-todo] se pasa el apikey al enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api-i27cnm7fja-uc.a.run.app',
+  apiKey: 'AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:3000/api',
+  apiKey: 'AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A',
 }
