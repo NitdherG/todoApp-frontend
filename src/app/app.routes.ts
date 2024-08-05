50c9f4d2c977f516af88971ee7e71a682bc1c4c3
NitdherG <nitdherdev@gmail.com>
2024-08-05 15:56:00 -0400

    [frontend-todo] se configura las rutas para le proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,27 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: "",
-        redirectTo: "/home",
-        pathMatch: "full"
-    },
-    {
-        path: "home",
-        loadComponent: () => import("./modules/example-page/example-page.component").then((m) => m.ExamplePageComponent)
-
-    }
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./modules/example-page/example-page.component').then(
+        (m) => m.ExamplePageComponent,
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/auth/components/login/login.component').then(
+        (m) => m.LoginComponent,
+      ),
+  },
+  {
+    path: 'todo',
+    loadComponent: () => import('./feature/todo/layout/todo-layout.component').then((m) => m.TodoLayoutComponent),
+  }
 ];
