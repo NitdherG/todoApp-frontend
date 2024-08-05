8df80d1db975f1eec3bbab36760ef9b68f7eafcd
NitdherG <nitdherdev@gmail.com>
2024-08-05 17:19:08 -0400

    [frontend-todo] se arregla la url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api-i27cnm7fja-uc.a.run.app',
+  baseUrl: 'https://api-i27cnm7fja-uc.a.run.app/api',
   apiKey: 'AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A',
 }
