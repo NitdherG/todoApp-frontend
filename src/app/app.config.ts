750b71eb48a84f8e9176232b6eaa258fd79480bf
NitdherG <nitdherdev@gmail.com>
2024-08-05 16:21:14 -0400

    [frontend-todo] se elimina el location id porque generaba problemas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"todoapp-fbc7d","appId":"1:911067566463:web:36f8a404bf0e5bcba04a4a","storageBucket":"todoapp-fbc7d.appspot.com","locationId":"us-central","apiKey":"AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A","authDomain":"todoapp-fbc7d.firebaseapp.com","messagingSenderId":"911067566463"})), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
+    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"todoapp-fbc7d","appId":"1:911067566463:web:36f8a404bf0e5bcba04a4a","storageBucket":"todoapp-fbc7d.appspot.com","apiKey":"AIzaSyCvY0oi_ZPnhThLKE9yrbLhw8jqLD1620A","authDomain":"todoapp-fbc7d.firebaseapp.com","messagingSenderId":"911067566463"})), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
 };
